f3830295e89747dcd00b73d43c31490b545a053a
pedro.pinho <pedro.pinho@luizalabs.com>
2021-02-09 13:28:10 -0300

    init repo

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-debugger, no-console*/
+import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/firestore";
+
+
+// Firebase configuration
+firebase.initializeApp({
+    apiKey: "AIzaSyDehU0Te8GFVtFCdf_gnhXjSzy2ECBmtgs",
+    authDomain: "ldl-app.firebaseapp.com",
+    projectId: "ldl-app",
+    storageBucket: "ldl-app.appspot.com",
+    messagingSenderId: "174290961793",
+    appId: "1:174290961793:web:74ab0e30143371a7f61480",
+    measurementId: "G-TJRRVK3RMZ"
+});
+
+// // Exporting Firebase instances
+// export const FA = firebase.auth();
+// export const FFS = firebase.firestore();
